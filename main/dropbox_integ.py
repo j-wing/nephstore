40556f95159277f9e02a6fd14ea077e4977e7526
Jordon Wing <jordonwii@gmail.com>
2012-05-27 09:43:27 -0700

    Implemented cd, ls, and mkdir on DropboxAPI class, as well as generic exec_command

--- main/dropbox_integ.py
+++ main/dropbox_integ.py
@@ -1,5 +1,6 @@
 from api_keys import DROPBOX_INFO
 import dropbox
+from dropbox.rest import ErrorResponse
 import time, os, pprint, logging
 from django.core.urlresolvers import reverse
 
@@ -49,3 +50,84 @@ class DropboxAPI(object):
         del self.request.session['dropbox_token_secret']
         
         return True
+    
+    def _init_client(self):
+        access_token = self.request.user.credentials.dropbox_access
+        
+        sess = dropbox.session.DropboxSession(*DROPBOX_INFO)
+        sess.set_token(*access_token)
+        client = dropbox.client.DropboxClient(sess)
+        return client
+        
+    def cd(self, path):
+        path = path
+        client = self._init_client()
+        
+        success,exists,is_dir, data = True, True,True,[]
+        try:
+            meta = client.metadata(path)
+        except ErrorResponse as e:
+            exists = False
+            success = False
+        else:
+            if not meta['is_dir']:
+                is_dir = False
+                success = False
+            else:
+                data = meta['contents']
+        return {
+            "success":success,
+            "exists":exists,
+            "is_dir":is_dir,
+            "data":data
+        }
+    
+    def mkdir(self, path, name):
+        full_path = os.path.join(path, name)
+        client = self._init_client()
+        
+        data = {
+            "success":True,
+            "exists_already":False,
+            "error":"",
+            "full_path":""
+        }
+        
+        try:
+            meta = client.file_create_folder(full_path)
+        except ErrorResponse as e:
+            if e.status == 403:
+                data['success'] = False
+                data['exists_already'] = True
+            else:
+                data['success'] = False
+                data['error'] = e.error_msg
+        else:
+            data['full_path'] = meta['path']
+        return data
+    
+    def ls(self, path):
+        client = self._init_client()
+        
+        data = {
+            "success":True,
+            "error":"",
+            "contents":[]
+        }
+        
+        try:
+            meta = client.metadata(path)
+        except ErrorResponse as e:
+            data['success'] = False
+            data['error'] = e.error_msg
+        else:
+            print meta
+            if meta['is_dir']:
+                data['contents'] = meta['contents']
+            else:
+                data['contents'] = [{"path":os.path.basename(path)}]
+        return data
+    def exec_command(self, command, *args, **kwargs):
+        if command == "cd": return self.cd(**kwargs)
+        elif command == "mkdir": return self.mkdir(**kwargs)
+        elif command == "ls":return self.ls(**kwargs)
