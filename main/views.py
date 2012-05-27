f11f6260301d3dfa4d2fdd526e0cd0568a92bb19
Jordon Wing <jordonwii@gmail.com>
2012-05-26 23:08:23 -0700

    Index page now sends CSRF cookie

--- main/views.py
+++ main/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from responses import JSONResponse
 from dropbox_integ import DropboxAPI
 from django.core.urlresolvers import reverse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
+@ensure_csrf_cookie()
 def index(request):
     return render(request, "terminal.html")
 
