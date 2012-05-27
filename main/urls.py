d3b00abf894298ac53915580023015114a404c64
Jordon Wing <jordonwii@gmail.com>
2012-05-27 09:44:46 -0700

    Added url for command view

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('nephstore.main.views',
     url(r'^$', "index", name="index"),
     url(r'^user/get/$', 'user_info', name="user_info"),
+    url(r'^command/$', 'command', name="command"),
     url(r'^dropbox_auth/$', 'dropbox_step1', name="dropbox_auth1"),
     url(r'^dropbox_oauth2_callback/$', 'dropbox_step2', name="dropbox_auth2"),
 )
